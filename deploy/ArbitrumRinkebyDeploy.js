1b4eff50304fea7a324090c51768d861dace68bb
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-06-29 19:42:45 +0200

    Update deploy script

--- deploy/ArbitrumRinkebyDeploy.js
+++ deploy/ArbitrumRinkebyDeploy.js
@@ -4,30 +4,52 @@ module.exports = async (hre) => {
     const { deployer } = await getNamedAccounts()
     const accounts = await ethers.getSigners()
 
-    const BTC_POOL_CODE = "BTC/USD"
-    const ETH_POOL_CODE = "ETH/USD"
-
     const DEPLOY_POOL_GAS_LIMIT = 10000000
 
     const POOL_DEFAULT_MINTING_FEE = ethers.utils.parseEther("0.015")
     const POOL_DEFAULT_BURNING_FEE = ethers.utils.parseEther("0.015")
-    const POOL_DEFAULT_FRONT_RUNNING_INTERVAL = 2400
-    const POOL_DEFAULT_UPDATE_INTERVAL = 300
     const POOL_DEFAULT_CHANGE_INTERVAL = "0"
 
     const HALF_LEVERAGE = 42
-    const ONE_LEVERAGE = 1
-    const THREE_LEVERAGE = 3
 
-    const SMA_DEFAULT_PERIODS = 24
-    const SMA_DEFAULT_UPDATE_INTERVAL = 300
+    const SMA_DEFAULT_PERIODS = 8
+    const SMA_DEFAULT_UPDATE_INTERVAL = 60 * 60
 
-    const arbitrumRinkEthUsdOracle = {
-        address: "0x5f0423B1a6935dc5596e7A24d98532b67A0AeFd8",
+    const ethUsdSpotPricePool = {
+        code: "ETH/USD+USDC-12h",
+        stylizedCode: "EthUsdSpotPrice",
+        oracleAddress: "0x5f0423B1a6935dc5596e7A24d98532b67A0AeFd8",
+        frontRunningInterval: 300,
+        updateInterval: 43200,
     }
-    const arbitrumRinkBtcUsdOracle = {
-        address: "0x0c9973e7a27d00e656B9f153348dA46CaD70d03d",
+    const ethUsd8hPool = {
+        code: "ETH/USD+USDC",
+        stylizedCode: "EthUsd8h",
+        oracleAddress: "0x5f0423B1a6935dc5596e7A24d98532b67A0AeFd8",
+        frontRunningInterval: 28800,
+        updateInterval: 3600,
     }
+    const btcUsd8hPool = {
+        code: "BTC/USD+USDC",
+        stylizedCode: "BtcUsd8h",
+        oracleAddress: "0x0c9973e7a27d00e656B9f153348dA46CaD70d03d",
+        frontRunningInterval: 28800,
+        updateInterval: 3600,
+    }
+    const btcEth8hPool = {
+        code: 'BTC/ETH+WETH',
+        stylizedCode: 'BtcEth8h',
+        oracleAddress: "0x6eFd3CCf5c673bd5A7Ea91b414d0307a5bAb9cC1",
+        frontRunningInterval: 28800,
+        updateInterval: 3600,
+    }
+
+    const pools = [
+        ethUsdSpotPricePool,
+        ethUsd8hPool,
+        btcUsd8hPool,
+        btcEth8hPool,
+    ]
 
     /* deploy testToken */
     const token = await deploy("TestToken", {
@@ -48,22 +70,6 @@ module.exports = async (hre) => {
         accounts[0].address,
         ethers.utils.parseEther("100000000") // 100 mil supply
     )
-
-    // base btc usd oracle wrapper
-    const btcOracleWrapper = await deploy("BtcUsdOracleWrapper", {
-        from: deployer,
-        log: true,
-        contract: "ChainlinkOracleWrapper",
-        args: [arbitrumRinkBtcUsdOracle.address, deployer],
-    })
-
-    // base eth usd oracle wrapper
-    const ethOracleWrapper = await deploy("EthUsdOracleWrapper", {
-        from: deployer,
-        log: true,
-        contract: "ChainlinkOracleWrapper",
-        args: [arbitrumRinkEthUsdOracle.address, deployer],
-    })
 
     // deploy PoolSwapLibrary
     const library = await deploy("PoolSwapLibrary", {
@@ -177,111 +183,17 @@ module.exports = async (hre) => {
         invariantCheck.address
     )
 
-    // deploy ETH SMA Oracle
-    const ethSmaOracleWrapper = await deploy("EthUsdSMAOracle", {
-        from: deployer,
-        log: true,
-        contract: "SMAOracle",
-        args: [
-            ethOracleWrapper.address, //Oracle Address
-            SMA_DEFAULT_PERIODS, // number of periods
-            SMA_DEFAULT_UPDATE_INTERVAL, // Update interval
-            deployer, // deployer address
-            deployer,
-            deployer,
-        ],
-    })
-
-    // Poll so there is an initial price
-    await execute(
-        "EthUsdSMAOracle",
-        {
+    // base wrappers
+    var oracleWrappers = []
+    for (const pool of pools) {
+        const oracleWrapper = await deploy(pool.stylizedCode + "OracleWrapper", {
             from: deployer,
             log: true,
-        },
-        "poll"
-    )
-
-    await execute(
-        "EthUsdSMAOracle",
-        {
-            from: deployer,
-            log: true,
-        },
-        "setPoolKeeper",
-        poolKeeper.address
-    )
-
-    const btcSmaOracleWrapper = await deploy("BtcUsdSMAOracle", {
-        from: deployer,
-        log: true,
-        contract: "SMAOracle",
-        args: [
-            btcOracleWrapper.address, //Oracle Address
-            SMA_DEFAULT_PERIODS, // number of periods
-            SMA_DEFAULT_UPDATE_INTERVAL, // Update interval
-            deployer, // deployer address
-            deployer,
-            deployer,
-        ],
-    })
-
-    // Poll so there is an initial price
-    await execute(
-        "BtcUsdSMAOracle",
-        {
-            from: deployer,
-            log: true,
-        },
-        "poll"
-    )
-
-    await execute(
-        "BtcUsdSMAOracle",
-        {
-            from: deployer,
-            log: true,
-        },
-        "setPoolKeeper",
-        poolKeeper.address
-    )
-
-    // deploy pools
-
-    // ETH-USD 0.5x
-    const deploymentData1 = {
-        poolName: ETH_POOL_CODE,
-        frontRunningInterval: POOL_DEFAULT_FRONT_RUNNING_INTERVAL,
-        updateInterval: POOL_DEFAULT_UPDATE_INTERVAL,
-        leverageAmount: HALF_LEVERAGE,
-        settlementToken: token.address,
-        oracleWrapper: ethSmaOracleWrapper.address,
-        settlementEthOracle: ethOracleWrapper.address,
-        feeController: deployer,
-        mintingFee: POOL_DEFAULT_MINTING_FEE,
-        burningFee: POOL_DEFAULT_BURNING_FEE,
-        changeInterval: POOL_DEFAULT_CHANGE_INTERVAL,
+            contract: "ChainlinkOracleWrapper",
+            args: [pool.oracleAddress, deployer],
+        })
+        oracleWrappers.push(oracleWrapper)
     }
-
-    // BTC-USD 0.5x
-    const deploymentData2 = {
-        poolName: BTC_POOL_CODE,
-        frontRunningInterval: POOL_DEFAULT_FRONT_RUNNING_INTERVAL,
-        updateInterval: POOL_DEFAULT_UPDATE_INTERVAL,
-        leverageAmount: HALF_LEVERAGE,
-        settlementToken: token.address,
-        oracleWrapper: btcSmaOracleWrapper.address,
-        settlementEthOracle: ethOracleWrapper.address,
-        feeController: deployer,
-        mintingFee: POOL_DEFAULT_MINTING_FEE,
-        burningFee: POOL_DEFAULT_BURNING_FEE,
-        changeInterval: POOL_DEFAULT_CHANGE_INTERVAL,
-    }
-
-    const deploymentData = [
-        deploymentData1,
-        deploymentData2,
-    ]
 
     console.log(`Deployed TestToken: ${token.address}`)
     console.log(`Deployed PoolFactory: ${factory.address}`)
@@ -290,7 +202,68 @@ module.exports = async (hre) => {
     console.log(`Deployed L2Encoder: ${l2Encoder.address}`)
     console.log(`Deployed PoolKeeper: ${poolKeeper.address}`)
 
-    for (var i = 0; i < deploymentData.length; i++) {
+    // deploy oracles
+    var smaOracleWrappers = []
+    for (const [i, pool] of pools.entries()) {
+        if (pool.frontRunningInterval < pool.updateInterval) {
+            smaOracleWrappers.push(oracleWrappers[i])
+            continue
+        }
+        const smaOracleWrapper = await deploy(pool.stylizedCode + "SMAOracle", {
+            from: deployer,
+            log: true,
+            contract: "SMAOracle",
+            args: [
+                oracleWrappers[i].address, // oracle address
+                SMA_DEFAULT_PERIODS, // number of periods
+                SMA_DEFAULT_UPDATE_INTERVAL, // update interval
+                deployer, // deployer address
+                deployer,
+                deployer,
+            ],
+        })
+        smaOracleWrappers.push(smaOracleWrapper)
+    }
+
+    // poll so there is an initial price
+    for (const pool of pools) {
+        if (pool.frontRunningInterval < pool.updateInterval) {
+            continue
+        }
+        await execute(
+            pool.stylizedCode + "SMAOracle",
+            {
+                from: deployer,
+                log: true,
+            },
+            "poll",
+        )
+        await execute(
+            pool.stylizedCode + "SMAOracle",
+            {
+                from: deployer,
+                log: true,
+            },
+            "setPoolKeeper",
+            poolKeeper.address
+        )
+    }
+
+    // deploy pools
+    for (const [i, pool] of pools.entries()) {
+        const deploymentData = {
+            poolName: pool.code,
+            frontRunningInterval: pool.frontRunningInterval,
+            updateInterval: pool.updateInterval,
+            leverageAmount: HALF_LEVERAGE,
+            settlementToken: token.address,
+            oracleWrapper: smaOracleWrappers[i].address,
+            settlementEthOracle: oracleWrappers[i].address,
+            feeController: deployer,
+            mintingFee: POOL_DEFAULT_MINTING_FEE,
+            burningFee: POOL_DEFAULT_BURNING_FEE,
+            changeInterval: POOL_DEFAULT_CHANGE_INTERVAL,
+        }
         let receipt = await execute(
             "PoolFactory",
             {
@@ -299,27 +272,12 @@ module.exports = async (hre) => {
                 gasLimit: DEPLOY_POOL_GAS_LIMIT,
             },
             "deployPool",
-            deploymentData[i]
+            deploymentData
         )
         const event = receipt.events.find((el) => el.event === "DeployPool")
-
         console.log(`Deployed LeveragedPool: ${event.args.pool}`)
         console.log(`Deployed PoolCommitter: ${event.args.poolCommitter}`)
     }
-
-    // Commented out because if fails if already verified. Need to only do it once or modify to not failed if already verified
-    // await hre.run("verify:verify", {
-    //     address: oracleWrapper.address,
-    //     constructorArguments: [arbitrumRinkBtcUsdOracle.address, deployer],
-    // })
-    // await hre.run("verify:verify", {
-    //     address: keeperOracle.address,
-    //     constructorArguments: [arbitrumRinkEthUsdOracle.address, deployer],
-    // })
-    // await hre.run("verify:verify", {
-    //     address: poolKeeper.address,
-    //     constructorArguments: [factory.address],
-    // })
 }
 
 module.exports.tags = ["ArbRinkebyDeploy"]
