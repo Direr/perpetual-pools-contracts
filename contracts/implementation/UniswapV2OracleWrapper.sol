de3b240c9774dde7ddbf1d7a681e954bde189aee
Weijia Wang <9713184+wegank@users.noreply.github.com>
2022-07-25 09:46:21 +0200

    Revert to spot price

--- contracts/implementation/UniswapV2OracleWrapper.sol
+++ contracts/implementation/UniswapV2OracleWrapper.sol
@@ -14,23 +14,12 @@ contract UniswapV2OracleWrapper is IOracleWrapper {
     address public immutable override deployer;
     uint8 private constant DECIMALS = 18;
 
-    bool public reversed;
-    uint256 public priceAverageWad;
-    uint256 public priceCumulativeLast;
-    uint32 public blockTimestampLast;
-
     // #### Functions
     constructor(address _oracle, address _deployer) {
         require(_oracle != address(0), "Oracle cannot be null");
         require(_deployer != address(0), "Deployer cannot be null");
         oracle = _oracle;
         deployer = _deployer;
-        // initialize the prices
-        (uint112 reserve0, uint112 reserve1, uint32 blockTimestamp) = IUniswapV2Pair(oracle).getReserves();
-        reversed = (reserve0 > reserve1);
-        priceAverageWad = reversed ? (_fromWad(reserve0)) / reserve1 : (_fromWad(reserve1)) / reserve0;
-        priceCumulativeLast = _priceCumulativeLast();
-        blockTimestampLast = blockTimestamp;
     }
 
     function decimals() external pure override returns (uint8) {
@@ -41,15 +30,27 @@ contract UniswapV2OracleWrapper is IOracleWrapper {
      * @notice Returns the oracle price in WAD format
      */
     function getPrice() external view override returns (int256) {
-        return int256(priceAverageWad);
+        (int256 _price, ) = _latestRoundData();
+        return _price;
     }
 
     /**
      * @return _price The latest round data price
-     * @return _data The metadata. Implementations can choose what data to return here. This implementation returns the blockTimestampLast
+     * @return _data The metadata. Implementations can choose what data to return here. This implementation returns the blockTimestamp
      */
     function getPriceAndMetadata() external view override returns (int256, bytes memory) {
-        return (int256(priceAverageWad), abi.encodePacked(uint80(blockTimestampLast)));
+        (int256 price, uint80 roundID) = _latestRoundData();
+        bytes memory _data = abi.encodePacked(roundID);
+        return (price, _data);
+    }
+
+    function _latestRoundData() internal view returns (int256, uint80) {
+        (uint112 reserve0, uint112 reserve1, uint32 blockTimestamp) = IUniswapV2Pair(oracle).getReserves();
+        if (reserve0 > reserve1) {
+            return (int256((reserve0 * (10**DECIMALS)) / reserve1), uint80(blockTimestamp));
+        } else {
+            return (int256((reserve1 * (10**DECIMALS)) / reserve0), uint80(blockTimestamp));
+        }
     }
 
     /**
@@ -59,30 +60,7 @@ contract UniswapV2OracleWrapper is IOracleWrapper {
         return wad * int256(10**DECIMALS);
     }
 
-    function _fromWad(uint256 wad) internal pure returns (uint256) {
-        return wad * (10**DECIMALS);
-    }
-
-    function _priceCumulativeLast() internal view returns (uint256) {
-        if (!reversed) {
-            return IUniswapV2Pair(oracle).price0CumulativeLast();
-        } else {
-            return IUniswapV2Pair(oracle).price1CumulativeLast();
-        }
-    }
-
-    function poll() external override returns (int256) {
-        (, , uint32 blockTimestamp) = IUniswapV2Pair(oracle).getReserves();
-        if (blockTimestamp > blockTimestampLast) {
-            uint256 priceCumulative = _priceCumulativeLast();
-            // update the price average
-            priceAverageWad = _fromWad(priceCumulative - priceCumulativeLast);
-            priceAverageWad /= (blockTimestamp - blockTimestampLast);
-            priceAverageWad >>= 112;
-            // update other globals
-            priceCumulativeLast = priceCumulative;
-            blockTimestampLast = blockTimestamp;
-        }
-        return int256(priceAverageWad);
+    function poll() external pure override returns (int256) {
+        return 0;
     }
 }
